461fb5bf64a9a1aedf74299a649837e08febad58
SonyaNazaryshyna <nazaryshynasofia@gmail.com>
2026-05-16 20:26:17 +0300

    Updated

--- tests/integration/test_commands.py
+++ tests/integration/test_commands.py
@@ -3,36 +3,36 @@ from io import StringIO
 from django.core.management import call_command
 from src.infrastructure.database.models import UserORM
 
+
 @pytest.mark.django_db
 class TestPromoteAdminCommand:
-    
+
     def test_promote_user_to_admin_success(self):
         username = "test_user"
-        UserORM.objects.create(username=username, role="ROLE_USER")
-        
+        UserORM.objects.create(username=username, email="test@test.com", role="ROLE_USER", is_active=True)
+
         out = StringIO()
-        call_command("make_admin", "testuser", stdout=out)
-        
-        assert f"@{username} is now ROLE_ADMIN" in out.getvalue()
-        
+        call_command("make_admin", username, stdout=out)
+
+        assert "ROLE_ADMIN" in out.getvalue()
         user = UserORM.objects.get(username=username)
         assert user.role == "ROLE_ADMIN"
 
     def test_promote_user_not_found(self):
         out = StringIO()
         username = "non_existent_user"
-        
-        call_command("make_admin", "testuser", stdout=out)
-        
-        assert f"User '{username}' not found" in out.getvalue()
+
+        call_command("make_admin", username, stdout=out)
+
+        assert "not found" in out.getvalue()
 
     def test_promote_already_admin(self):
         username = "already_admin"
-        UserORM.objects.create(username=username, role="ROLE_ADMIN")
-        
+        UserORM.objects.create(username=username, email="admin@test.com", role="ROLE_ADMIN", is_active=True)
+
         out = StringIO()
-        call_command("make_admin", "testuser", stdout=out)
-        
-        assert f"@{username} is now ROLE_ADMIN" in out.getvalue()
+        call_command("make_admin", username, stdout=out)
+
+        assert "ROLE_ADMIN" in out.getvalue()
         user = UserORM.objects.get(username=username)
         assert user.role == "ROLE_ADMIN"
